6ffec572e7b7036bf0ddb74513f5e93f83b9c09a
1851629 <1851629@01-19753.pusd.dom>
2023-03-21 13:46:17 -0700

    dfghjk

---
+++ Sim/src/Cell.java
@@ -0,0 +1,106 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Cell {
+
+	private int x, y;
+	private int vx, vy;
+	private int status = 0; // healthy is 0
+	private int time = 5000;
+	private int time2 = 5000;
+	private int chance;
+	private boolean recovered = false;
+	private boolean prime = false;
+
+	public Cell(int initX, int initY) {
+		this();
+		prime = true;
+		status = 1;
+		x = initX;
+		y = initY;
+
+	}
+
+	public Cell() {
+
+		x = (int) (Math.random() * 501);
+		y = (int) (Math.random() * 501);
+		vx = (int) (Math.random() * 9) - 4;
+		vy = (int) (Math.random() * 9) - 4;
+
+		while (vx == 0) {
+			vx = (int) (Math.random() * 9) - 4;
+		}
+		while (vy == 0) {
+			vy = (int) (Math.random() * 9) - 4;
+		}
+
+	}
+
+	public void intersect(Cell other) {
+
+		Rectangle og = new Rectangle(x, y, 25, 25);
+		Rectangle otherCell = new Rectangle(other.x, other.y, 25, 25);
+
+		if (og.intersects(otherCell)) {
+			if (this.recovered == false && other.recovered == false) {
+				if (this.status == 1 && other.status == 0) {
+					other.status = 1;
+				} else if (this.status == 0 && other.status == 1) {
+					this.status = 1;
+				}
+
+			}
+		}
+
+	}
+
+	public void paint(Graphics g) {
+		if (status == 0) {
+			g.setColor(Color.blue);
+			if (recovered) {
+				g.setColor(Color.green);
+				time2 -= 16;
+				if (time2 <= 0) {
+					recovered = false;
+					time2 = 5000;
+				}
+			}
+
+		} else if (status == 1) {
+			g.setColor(Color.black);
+			if (prime == false) {
+				time -= 16;
+				if (time <= 0) {
+					time = 5000;
+					chance = (int) (Math.random() * 100);
+
+					if (chance < 50) {
+						x = 10000;
+						y = 10000;
+						vx = 0;
+						vy = 0;
+					} else {
+						g.setColor(Color.green);
+						status = 0;
+						recovered = true;
+					}
+				}
+			}
+
+		}
+		g.fillOval(x, y, 30, 30);
+		x += vx;
+		y += vy;
+
+		if (x < 0 || x >= 775) {
+			vx *= -1;
+		}
+		if (y < 0 || y >= 575) {
+			vy *= -1;
+		}
+
+	}
+
+}
---
+++ Sim/src/Frame.java
@@ -0,0 +1,108 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Frame extends JPanel implements KeyListener, ActionListener{
+	ArrayList<Cell> cells = new ArrayList<Cell>();
+	// instance variables - "global" variables
+	
+	int x = 0;
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		
+		for(int i = 0;i<cells.size();i++) {
+			
+			(cells.get(i)).paint(g);
+		
+			
+			
+			
+			
+			
+		}
+		
+		
+		for(int i =0;i<cells.size();i++) {
+			for(int j = 0;j<cells.size();j++) {
+				
+				cells.get(i).intersect(cells.get(j));
+				
+			}
+			
+		}
+		
+		
+		
+		
+	}
+	
+	
+	public Frame() {
+		//Name of title bar of the GUI
+		JFrame f = new JFrame("Sim");
+		
+		//makes sure that the program stops when the window is close
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //don't delete
+		
+		//GUI dimensions - width - height
+		f.setSize(800,600);
+		
+		//create 100 cells
+		
+		for(int i=0;i<101;i++) {
+			cells.add(new Cell());
+		}
+		
+		
+		
+		
+		f.add(this);
+		f.addKeyListener(this);
+		
+		t = new Timer(16, this);
+		 t.start();
+		f.setVisible(true);
+		
+	}
+	
+	
+	
+	
+	Timer t;
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		 cells.add(new Cell(350,250));
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] arg) {
+		Frame f = new Frame();
+	}
+}
